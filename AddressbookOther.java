cb8138ee0905f79699f24a283a91f85e6be04db9
JEN6YT <71663327+JEN6YT@users.noreply.github.com>
2020-11-19 17:28:02 -0500

    Add files via upload

---
+++ AddressbookOther.java
@@ -0,0 +1,22 @@
+package Q2;
+
+/**
+ * 
+ * @author jenniferzhang
+ *
+ */
+
+// Addressbook Other extends Addressbook
+class AddressbookOther extends Addressbook{
+	
+	/**
+	 * 
+	 * @return number of entries
+	 */
+	public int getNumOfEntries() {
+		return numOfEntries;
+	}
+	
+	
+
+}
